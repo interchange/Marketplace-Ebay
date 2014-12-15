1e92256f472f4b4f064cbc3c8003387fa4682837
Marco Pessotto <melmothx@gmail.com>
2014-12-15 14:58:55 +0100

    Initial commit with module-starter output

---
+++ lib/Marketplace/Ebay.pm
@@ -0,0 +1,111 @@
+package Marketplace::Ebay;
+
+use 5.010001;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Marketplace::Ebay - The great new Marketplace::Ebay!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Marketplace::Ebay;
+
+    my $foo = Marketplace::Ebay->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Marco Pessotto, C<< <melmothx at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-marketplace-ebay at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Marketplace-Ebay>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Marketplace::Ebay
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Marketplace-Ebay>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Marketplace-Ebay>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Marketplace-Ebay>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Marketplace-Ebay/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Marco Pessotto.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+
+=cut
+
+1; # End of Marketplace::Ebay
