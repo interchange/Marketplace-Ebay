7481ac26a5b1742a74ccb7c212e154a8dc90eb7e
Marco Pessotto <melmothx@gmail.com>
2015-06-17 13:39:16 +0200

    version bump

--- lib/Marketplace/Ebay.pm
+++ lib/Marketplace/Ebay.pm
@@ -24,11 +24,11 @@ Marketplace::Ebay - Making API calls to eBay (with XSD validation)
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 =head1 SYNOPSIS
 
